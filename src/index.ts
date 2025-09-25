8cfcf36be7818a8a03a5f8c3a23269a43b40c0d4
Noman Haider <noman567n@gmail.com>
2025-09-25 11:09:04 +0500

    remove unused modules

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import express from 'express';
 import { connectMongoDB } from './config/database';
-import { envs } from './config/envs';
 import contactEmailRoute from './routes/email';
 import cors from 'cors';
 
--- src/models/user.models.ts
+++ src/models/user.models.ts
@@ -1,4 +1,3 @@
-// models/User.ts
 import mongoose, { Schema } from "mongoose";
 import { IWorkflowType } from "./workflow.models";
 
